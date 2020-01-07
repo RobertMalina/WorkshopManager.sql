a49ae10ecd97478af4601a883d0d76b3e895e4c1
Robert <robert.malina987@gmail.com>
2020-01-07 20:18:43 +0100

    sp for querying active orders (with LEFT JOIN, so no supervised orders are returned too)

---
+++ GetNonArchivedOrdersData.sql
@@ -0,0 +1,31 @@
+IF  EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[GetOrderDataWithId]') AND type in (N'P', N'PC'))  
+	DROP PROCEDURE [dbo].[GetOrderDataWithId]
+GO
+CREATE PROCEDURE [dbo].[GetOrderDataWithId] @orderId int
+AS
+BEGIN
+	SELECT O.Id, 
+		  O.SupervisorId, 
+		  O.Title, 
+		  O.VehicleDescription, 
+		  O.Description, 
+		  O.DateRegister, 
+		  O.DateStart,
+		  O.DateEnd, 
+		  O.Cost, 
+		  O.EstimatedTime, 
+		  O.Status,
+		  (SELECT C.Id FROM [Client] C where C.Id = O.ClientId) AS [Client.Id],
+		  (SELECT C.FirstName FROM [Client] C where C.Id = O.ClientId) AS [Client.FirstName],
+		  (SELECT C.LastName FROM [Client] C where C.Id = O.ClientId) AS [Client.LastName],
+		  (SELECT C.PhoneNumber FROM [Client] C where C.Id = O.ClientId) AS [Client.PhoneNumber],
+			W.FirstName AS [Supervisor.FirstName],
+		  W.LastName AS [Supervisor.LastName],
+		  W.PhoneNumber AS [Supervisor.PhoneNumber]
+			FROM [Order] O
+			INNER JOIN [Worker] W on O.SupervisorId = W.Id
+			WHERE O.Archived = 0;
+END
+
+--EXEC GetOrderDataWithId @orderId = 9;
